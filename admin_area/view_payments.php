26bea9e7b387fb7a548c7a9036d74ccd2f9967f5
panagiip <panagiip@gmail.com>
2018-05-10 15:07:29 +0300

    view_payments.php (2)

--- admin_area/view_payments.php
+++ admin_area/view_payments.php
@@ -40,11 +40,54 @@ else {
 									<th>Αριθμός Τιμολογίου</th>
 									<th>Πληρωμένο Ποσό</th>
 									<th>Αριθμός Αναφοράς</th>
+									<th>Κωδικός Πληρωμής</th>
 									<th>Ημερομηνία Πληρωμής</th>
 									<th>Διαγραφή Πληρωμής</th>
 								</tr>
 							</thead>
 							<tbody>
+
+								<?php
+
+								$i = 0;
+
+								$get_payments = "select * from payments";
+
+								$run_payments = mysqli_query($con,$get_payments);
+
+								while($row_payments = mysqli_fetch_array($run_payments)){
+
+									$payment_id = $row_payments['payment_id'];
+									$invoice_no = $row_payments['invoice_no'];
+									$amount = $row_payments['amount'];
+									$ref_no = $row_payments['ref_no'];
+									$code = $row_payments['code'];
+									$payment_date = $row_payments['payment_date'];
+
+									
+									$i++;
+
+								?>
+
+									<tr>
+										<td> <?php echo $i; ?></td>
+										<td bgcolor="yellow"> <?php echo $invoice_no;  ?></td>
+										<td> <?php echo $amount; ?>€</td>
+										<td><?php echo $ref_no; ?></td>
+								
+										
+										<td><?php echo $code; ?></td>
+										<td><?php echo $payment_date; ?></td>
+
+										<td>
+											<a href="index.php?payment_delete=<?php echo $payment_id; ?>">
+												<i class="fas fa-trash-alt"></i> Διαγραφή
+										</td>
+									</tr>
+
+								<?php  } ?>
+
+							</tbody>
 				</div>
 			</div>
 	</div>
