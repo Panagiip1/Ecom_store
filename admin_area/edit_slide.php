fb7202186f0dadd8bb2dc6931c24f875d516416c
panagiip <panagiip@gmail.com>
2018-05-27 12:02:13 +0300

    add url in the update admin

--- admin_area/edit_slide.php
+++ admin_area/edit_slide.php
@@ -29,7 +29,7 @@ if(isset($_GET['edit_slide'])){
 
 	$slide_image = $row_edit['slide_image'];
 
-
+	$slide_url = $row_edit['slide_url'];
 }
 
 ?>
@@ -71,6 +71,14 @@ if(isset($_GET['edit_slide'])){
 	    					<img src="slides_images/<?php echo $slide_image; ?>" width="70" height="55" style="float:left">
 	    				</div>
 	    			</div>
+
+	    			<div class="form-group row">
+	    				<label class="col-md-3 control-label">Slide Url</label>
+	    				<div class="col-md-8">
+	    					<input type="text" name="slide_url" value="<?php echo $slide_url; ?>" class="form-control"  required>
+	    				</div>
+	    			</div>
+
 	    			<div class="form-group row">
 	    				<label class="col-md-3 control-label"></label>
 	    				<div class="col-md-8">
@@ -94,9 +102,11 @@ if(isset($_POST['update'])){
 
 	$temp_name = $_FILES['slide_image']['tmp_name'];
 
+	$slide_url = $_POST['slide_url'];
+
 	move_uploaded_file($temp_name, "slides_images/$slide_image");
 
-	$update_slide = "update slider set slide_name='$slide_name',slide_image='$slide_image' where slide_id='$slide_id'";
+	$update_slide = "update slider set slide_name='$slide_name',slide_image='$slide_image',slide_url='$slide_url' where slide_id='$slide_id'";
 
 	$run_slide = mysqli_query($con,$update_slide);
 
